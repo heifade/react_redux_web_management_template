c10c25e09551085a2a52ea8eaf52467e65cbbb00
heifade <heifade@126.com>
2018-08-11 07:47:28 +0800

    no message

--- src/module/userManage/userList/userListService.ts
+++ src/module/userManage/userList/userListService.ts
@@ -1,9 +1,8 @@
 import { wait } from "../../../app/utils";
-import Item from "../../../../node_modules/antd/lib/list/Item";
 
 export let userListService = {
   async init() {
-    await wait(500);
+    await wait(300);
     let list = new Array<any>();
     for (let i = 0; i < 1000; i++) {
       list.push({ id: `${i}`, name: `name${i}` });
@@ -13,25 +12,22 @@ export let userListService = {
   },
 
   async getUserList() {
-    await wait(500);
+    await wait(300);
     return JSON.parse(window.localStorage.getItem("userList"));
   },
 
   async getUser(id: number) {
-    await wait(500);
     let userList = await this.getUserList();
     return userList.find((item: any) => item.id == id);
   },
 
   async deleteUser(id: number) {
-    await wait(500);
     let userList = await this.getUserList();
     let userList2 = userList.filter((item: any, index: number) => item.id !== id);
     window.localStorage.setItem("userList", JSON.stringify(userList2));
   },
 
   async saveUser(userData: any) {
-    await wait(500);
     let userList = await this.getUserList();
     let user = userList.find((item: any) => item.id === userData.id);
     user.name = userData.name;
